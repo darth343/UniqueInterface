df8c070301187d9e21ff926f216a4b9866408738
TemplarDon <donovandenzel@gmail.com>
2017-02-01 05:27:55 +0800

    MissionScreen and MissionSelect Screen

---
+++ Assets/Scripts/MissionSelector.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MissionSelector : MonoBehaviour {
+
+    public Text CurrMission;
+
+    private int MissionNumber;
+    private Vector2 startPos;
+    private Vector2 direction;
+    private Vector2 endPos;
+
+
+	// Use this for initialization
+	void Start () {
+        MissionNumber = int.Parse(CurrMission.text);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.touchCount > 1)
+        {
+           Touch touch = Input.GetTouch(0);
+
+			// Handle what to do based on touch phase
+			switch (touch.phase) 
+            {
+				case TouchPhase.Began:
+					startPos = touch.position;
+					break;
+				
+				case TouchPhase.Ended:
+                    // If dir is going left
+                    if (endPos.x < startPos.x)
+                    {
+                        IncrementWorld(true);
+                    }
+                    // If dir is going right
+                    else 
+                    {
+                        IncrementWorld(false);
+                    }
+
+					break;
+			}
+		}
+	}
+
+    public void IncrementWorld(bool isIncrement)
+    {
+        if (isIncrement)
+        {
+            MissionNumber++;
+
+            if (MissionNumber > 10)
+                MissionNumber = 1;
+        }
+        else
+        {
+            MissionNumber--;
+
+            if (MissionNumber <= 0)
+                MissionNumber = 10;
+        }
+        CurrMission.text = "World " + MissionNumber.ToString();
+    }
+}
--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -79,6 +79,49 @@ public class SettingsManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        // Sync settings with SharedData
+        GraphicalLevel = sharedData.GetInstance().GraphicalLevel;
+        debug.text = GraphicalLevel;
+
+        if (GraphicalLevel == "Low")
+        {
+            GameObject.Find("LowToggle").GetComponent<Toggle>().isOn = true;
+            GameObject.Find("MedToggle").GetComponent<Toggle>().isOn = false;
+            GameObject.Find("HighToggle").GetComponent<Toggle>().isOn = false;
+        }
+        else if (GraphicalLevel == "Med")
+        {
+            GameObject.Find("LowToggle").GetComponent<Toggle>().isOn = false;
+            GameObject.Find("MedToggle").GetComponent<Toggle>().isOn = true;
+            GameObject.Find("HighToggle").GetComponent<Toggle>().isOn = false;
+        }
+        else if (GraphicalLevel == "High")
+        {
+            GameObject.Find("LowToggle").GetComponent<Toggle>().isOn = false;
+            GameObject.Find("MedToggle").GetComponent<Toggle>().isOn = false;
+            GameObject.Find("HighToggle").GetComponent<Toggle>().isOn = true;
+        }
+
+
+        IsPowerSaver = sharedData.GetInstance().IsPowerSaver;
+        GameObject.Find("PowerToggle").GetComponent<Toggle>().isOn = IsPowerSaver;
+
+        SoundLevel = sharedData.GetInstance().SoundLevel;
+        GameObject.Find("SoundSlider").GetComponent<Slider>().value = SoundLevel;
+
+        MusicLevel = sharedData.GetInstance().MusicLevel;
+        GameObject.Find("MusicSlider").GetComponent<Slider>().value = MusicLevel;
+
+        IsNotification = sharedData.GetInstance().IsNotification;
+        GameObject.Find("NotificationToggle").GetComponent<Toggle>().isOn = IsNotification;
+
+        IsInvert = sharedData.GetInstance().IsInvert;
+        GameObject.Find("InvertToggle").GetComponent<Toggle>().isOn = IsInvert;
+
+        IsVibrate = sharedData.GetInstance().IsVibrate;
+        GameObject.Find("VibrateToggle").GetComponent<Toggle>().isOn = IsVibrate;
+
 	}
 
     public void SetActiveSetting(string settingName)
