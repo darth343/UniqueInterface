8c59f4834e1f63f6d3fc253c62de89c9327a501b
TemplarDon <donovandenzel@gmail.com>
2017-02-01 06:32:42 +0800

    INGAMEGUI

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ReduceHP(1);
+        ReduceMP(1);
+	}
+
+    public void ReduceHP(int amt)
+    {
+        GameObject.Find("TestHP").GetComponent<Slider>().value += amt;
+    }
+
+    public void ReduceMP(int amt)
+    {
+        GameObject.Find("TestMP").GetComponent<Slider>().value += amt;
+    }
+}
